522c3428fec7694009d111fc4994e899eefbc7f1
MrWalium <coolcoderai123@gmail.com>
2024-10-05 18:05:00 -0700

    Commiting my first file in my first branch.

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/WilliamsFile.java
@@ -0,0 +1,6 @@
+/* This is WilliamsFile.java in WilliamsBranch.
+ * The sole purpose of this file and branch
+ * is to practice using git in the commandline and bash
+ * and do use normal windows commandline
+ * lets go see if it worked!
+ */
