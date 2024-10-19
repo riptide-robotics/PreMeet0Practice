6b1fcf7353ddaa4954f778c27a16781c0a2cc8d7
J A <james.atzberger@gmail.com>
2024-10-18 21:08:10 -0700

    Commiting odomitor and field centric drive class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
@@ -0,0 +1,124 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp(name = "Field Centric Drive")
+public class FieldCentricDrive extends LinearOpMode {
+
+
+    // Stating dc motors
+    DcMotor flWheel;
+    DcMotor frWheel;
+    DcMotor blWheel;
+    DcMotor brWheel;
+//
+//    DcMotor frOdomitor;
+//    DcMotor flOdomitor;
+//    DcMotor fOdomitor;
+//
+
+
+
+
+    double x;
+    double y;
+    double rx;
+
+
+    private final double R = 4.8;
+    private final int V = 2000;
+    private final double C = 1/V;
+    private final double L = 10; //unknown I PUT A RANDOM NUMBER
+
+
+    private IMU imu;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // reference our motors so program knows what we calling
+        flWheel = hardwareMap.dcMotor.get("flWheel");
+        frWheel = hardwareMap.dcMotor.get("frWheel");
+        blWheel = hardwareMap.dcMotor.get("blWheel");
+        brWheel = hardwareMap.dcMotor.get("brWheel");
+
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        IMU.Parameters paremeters = new IMU.Parameters(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.LEFT));
+
+        imu.resetYaw();
+
+        imu.initialize(paremeters);
+
+
+
+        int odomitorParallel1Init = flWheel.getCurrentPosition() ;
+        int odomitorParallel2Init = frWheel.getCurrentPosition();
+        int odomitorPerpendicularInit = blWheel.getCurrentPosition();
+
+        waitForStart();
+
+        // Reverse direction because gamepad standard is reversed (only for left side)
+        flWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        blWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        while (opModeIsActive()) {
+            // referencing gamepad stick to varialbe (x multiplied by 1.1 for margine of error
+            y = -gamepad1.left_stick_y;
+            x = -gamepad1.left_stick_x * 1.1;
+            rx = gamepad1.right_stick_x;
+
+
+            double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double rotX = x * Math.cos(-heading) - y * Math.sin(-heading);
+            double rotY = x * Math.sin(-heading) + y * Math.sin(-heading);
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+
+
+
+            double scale = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+            // powers of each motor
+            double flPower = (rotY - rotX + rx) / denominator;
+            double frPower = (rotY + rotX - rx) / denominator;
+            double blPower = (rotY + rotX + rx) / denominator;
+            double brPower = (rotY - rotX - rx) / denominator;
+
+
+            // Set power to each motor
+            flWheel.setPower(flPower);
+            frWheel.setPower(frPower);
+            blWheel.setPower(blPower);
+            brWheel.setPower(brPower);
+
+            int odomitorParallel1Change = flWheel.getCurrentPosition() - odomitorParallel1Init;
+            int odomitorParallel2Change = frWheel.getCurrentPosition() - odomitorParallel2Init;
+            int odomitorPerpendicularChange = blWheel.getCurrentPosition() - odomitorPerpendicularInit;
+
+            frWheel.getCurrentPosition();
+            brWheel.getCurrentPosition();
+
+            double deltaY = odomitorPerpendicularChange - (odomitorParallel2Change - odomitorParallel1Change) / 2;
+            double deltaX = C*(odomitorParallel1Change + odomitorParallel2Change);
+            double deltaTheta = C*(odomitorParallel2Change - odomitorParallel1Change) / L;
+
+        }
+
+
+    }
+}
