7bba0592e2be4b2e9f03cfa7fede8099f1238c53
TreeSquirrles <herkyaaron@gmail.com>
2024-10-21 18:18:00 -0700

    added right slide functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideRotate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideRotate.java
@@ -2,19 +2,21 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Slide Move")
 public class SlideRotate extends LinearOpMode {
     Servo slideJoint;
-    final double rotateUp = 0; //idk the value
-    final double rotateDown = 0; // idk the value
+    final double rotateUp = 0.7; //idk the value
+    final double rotateDown = 0.4; // idk the value
+
+    DcMotor rLiftSlides;
+
     @Override
-
-
-
     public void runOpMode() throws InterruptedException{
         slideJoint = hardwareMap.servo.get("slideJoint");
+        rLiftSlides = hardwareMap.dcMotor.get("rSlide");
 
         waitForStart();
         while(opModeIsActive()){
@@ -31,8 +33,24 @@ public class SlideRotate extends LinearOpMode {
 
             if (gamepad1.dpad_left)
             {
-                slideJoint.setPosition(0);
+                slideJoint.setPosition(0.5);
             }
+
+            if (gamepad1.y)
+            {
+                rLiftSlides.setPower(1);
+            }
+
+            if (gamepad1.a)
+            {
+                rLiftSlides.setPower(-1);
+            }
+
+            if (gamepad1.b)
+            {
+                rLiftSlides.setPower(0);
+            }
+
 
 
         }
