5d11a44b4de00e07afee1ab69abf85201d859ecc
J A <james.atzberger@gmail.com>
2024-10-21 18:33:51 -0700

    Added a few changes, made a way to lift slides

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideRotate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideRotate.java
@@ -2,19 +2,30 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Slide Move")
 public class SlideRotate extends LinearOpMode {
     Servo slideJoint;
-    final double rotateUp = 0; //idk the value
-    final double rotateDown = 0; // idk the value
+    DcMotor slideMotor;
+    final double rotateUp = 0.7;
+    final double rotateDown = 0.3;
+    final double rotateMiddle = 0.5;
+
+    final int highestPos = 2350; //idk the value
+    final int lowestPos = 100;
+
     @Override
 
 
 
     public void runOpMode() throws InterruptedException{
-        slideJoint = hardwareMap.servo.get("slideJoint");
+        slideJoint = hardwareMap.servo.get("pivotSlide");
+
+        slideMotor = hardwareMap.dcMotor.get("slideMotor");
+
+        slideMotor.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
         while(opModeIsActive()){
@@ -34,7 +45,26 @@ public class SlideRotate extends LinearOpMode {
                 slideJoint.setPosition(0);
             }
 
+            if (gamepad1.dpad_left)
+            {
+                moveSlides(1, highestPos);
+            }
 
+            if (gamepad1.dpad_right)
+            {
+                moveSlides(-1, lowestPos);
+            }
+            if (gamepad1.left_bumper)
+            {
+                slideJoint.setPosition(rotateMiddle);
+            }
         }
+    }
+
+    public void moveSlides(double power, int target)
+    {
+        slideMotor.setTargetPosition(target);
+        slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slideMotor.setPower(power);
     }
 }
