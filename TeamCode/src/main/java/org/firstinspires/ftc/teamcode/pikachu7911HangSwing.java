f847a93883c98270c804238b197938a426f4be8f
pikachu7911 <young.seungyoo.kj@gmail.com>
2024-10-26 20:40:56 -0700

    I hate switchint T-T

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pikachu7911HangSwing.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class pikachu7911HangSwing extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
