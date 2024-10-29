08cb40e4b7012eb77a512a7bb40b10ac3b11440c
pikachu7911 <young.seungyoo.kj@gmail.com>
2024-10-28 19:47:18 -0700

    slides outtering and innering and upping and downing stuff done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pikachu7911HangSwing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pikachu7911HangSwing.java
@@ -1,36 +1,73 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class pikachu7911HangSwing extends LinearOpMode {
 
+    static double outestOut = 0.7;
+    static double innestIn = -0.35;
+
+    static int highestHigh = 2300;
+    static int lowestLow = 800;
+
     Servo leftHang;
     Servo rightHang;
+
+    DcMotor leftSlide;
+    DcMotor rightSlide;
 
     @Override
     public void runOpMode() throws InterruptedException {
         leftHang = hardwareMap.servo.get("leftHang");
         rightHang = hardwareMap.servo.get("rightHang");
 
+        leftSlide = hardwareMap.dcMotor.get("leftSlide");
+        rightSlide = hardwareMap.dcMotor.get("rightSlide");
+
         leftHang.setDirection(Servo.Direction.REVERSE);
         rightHang.setDirection(Servo.Direction.FORWARD);
+
+        leftSlide.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightSlide.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        leftSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
 
         while (opModeIsActive()) {
             if (gamepad2.left_bumper) {
-                leftHang.setPosition(0.7);
-                rightHang.setPosition(0.7);
+                leftHang.setPosition(outestOut);
+                rightHang.setPosition(outestOut);
             }
             if (gamepad2.right_bumper) {
-                leftHang.setPosition(-0.35);
-                rightHang.setPosition(-0.35);
+                leftHang.setPosition(innestIn);
+                rightHang.setPosition(innestIn);
             }
             if (gamepad2.dpad_down) {
                 leftHang.setPosition(leftHang.getPosition());
                 rightHang.setPosition(rightHang.getPosition());
-            }s
+            }
+            if (gamepad2.y) {
+                leftSlide.setTargetPosition(highestHigh);
+                rightSlide.setTargetPosition(highestHigh);
+                leftSlide.setPower(1);
+                rightSlide.setPower(1);
+            }
+
+            if (gamepad2.a) {
+                leftSlide.setTargetPosition(lowestLow);
+                rightSlide.setTargetPosition(lowestLow);
+                leftSlide.setPower(-1);
+                rightSlide.setPower(-1);
+            }
+            if (gamepad2.b) {
+                leftSlide.setPower(0);
+                rightSlide.setPower(0);
+            }
         }
     }
 }
