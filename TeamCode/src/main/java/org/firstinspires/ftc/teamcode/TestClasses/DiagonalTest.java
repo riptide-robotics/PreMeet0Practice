e2e42b0971cb03d80ab963a1d54bd7c511fcc538
pikachu7911 <young.seungyoo.kj@gmail.com>
2024-11-01 20:07:14 -0700

    diagtest stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClasses/DiagonalTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClasses/DiagonalTest.java
@@ -29,12 +29,18 @@ public class DiagonalTest extends LinearOpMode { // Edit for diagonal
 
             trapezoidalMotionProfile.calculateProfile(endPos);
             expectedPos = trapezoidalMotionProfile.getExpectedPosition(pidController.getTime());
+            // getting x & y of expectedPos
 
-            double currPos = Math.sqrt(Math.pow(robot.getCurrPos().getX(DistanceUnit.CM), 2) + Math.pow(robot.getCurrPos().getY(DistanceUnit.CM), 2));
-            double err = expectedPos - currPos;
-            double output = pidController.calculate(err);
+            double currX = robot.getCurrPos().getX(DistanceUnit.CM);
+            double currY = robot.getCurrPos().getY(DistanceUnit.CM);
 
-            robot.setWheelPowers(output, -output, output, -output); // What to put into this?
+            //double currPos = Math.sqrt(Math.pow(currX, 2) + Math.pow(currY, 2));
+            double currAngle = Math.atan2(currY, currX);
+
+//            double err = expectedPos - currPos;
+//            double output = pidController.calculate(err);
+//
+//            robot.setWheelPowers(output, -output, output, -output); // What to put into this?
         }
     }
 }
