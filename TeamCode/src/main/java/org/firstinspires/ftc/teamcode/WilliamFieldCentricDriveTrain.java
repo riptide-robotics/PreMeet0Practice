9e8d5c38f5708b9e180f00631c480e806d09caf6
MrWalium <coolcoderai123@gmail.com>
2024-11-01 18:43:11 -0700

    A skeleton of the controls for the linkage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WilliamFieldCentricDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WilliamFieldCentricDriveTrain.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -11,6 +12,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
+@TeleOp(name="Drive")
 public class WilliamFieldCentricDriveTrain extends LinearOpMode {
     DcMotor flWheelMotor;
     DcMotor frWheelMotor;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WilliamIntakeSkeleton.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="Intake Skeleton")
+public class WilliamIntakeSkeleton extends LinearOpMode {
+    CRServo crServoLeft;
+    CRServo crServoRight;
+
+    Servo extendServoLeft;
+    Servo extendServoRight;
+    Servo intakePitchServo;
+    Servo angleIntakeServoLeft;
+    Servo angleIntakeServoRight;
+
+    private final double minExtend = 0 /*Unknown*/;
+    private final double maxExtend = 0 /*Unknown*/;
+    private final double minPitch = 0 /*Unknown*/;
+    private final double maxPitch = 0 /*Unknown*/;
+    private final double minAngle = 0 /*Unknown*/;
+    private final double maxAngle = 0 /*Unknown*/;
+
+
+
+    public void runOpMode() {
+        crServoLeft = hardwareMap.crservo.get("crservo1"); // configure this
+        crServoRight = hardwareMap.crservo.get("crservo2");
+
+        extendServoLeft = hardwareMap.servo.get("extendservoleft");
+        extendServoRight = hardwareMap.servo.get("extendservoright");
+        intakePitchServo = hardwareMap.servo.get("pitchServo");
+        angleIntakeServoLeft = hardwareMap.servo.get("angleServol");
+        angleIntakeServoRight = hardwareMap.servo.get("angleServor");
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            if(gamepad1.left_bumper) {
+                crServoLeft.setPower(1);
+                crServoRight.setPower(1);
+            } else if(gamepad1.right_bumper) {
+                crServoLeft.setPower(-1);
+                crServoRight.setPower(-1);
+            } else {
+                crServoLeft.setPower(0);
+                crServoRight.setPower(0);
+            }
+
+            if(gamepad1.dpad_down) {
+                extendServoLeft.setPosition(0);
+                extendServoRight.setPosition(0);
+            }
+
+            if(gamepad1.dpad_up) {
+                extendServoLeft.setPosition(maxExtend);
+                extendServoRight.setPosition(maxExtend);
+            }
+
+            if(gamepad1.dpad_left) {
+                angleIntakeServoLeft.setPosition(minAngle);
+                angleIntakeServoRight.setPosition(minAngle);
+            }
+
+            if(gamepad1.dpad_right) {
+                angleIntakeServoLeft.setPosition(maxAngle);
+                angleIntakeServoRight.setPosition(maxAngle);
+            }
+
+            if(gamepad1.left_trigger >= 0.1) {
+                intakePitchServo.setPosition(minPitch);
+            }
+
+            if(gamepad1.right_trigger >= 0.1) {
+                intakePitchServo.setPosition(maxPitch);
+            }
+        }
+    }
+}
